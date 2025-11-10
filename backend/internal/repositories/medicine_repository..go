2743c152a19e602ff58e7ff2a0c7c820d6bc9a81
lutfiya ainurrahman <lutfiyapr.creative@gmail.com>
2025-11-10 21:17:40 +0700

    feat: add medicine repository

---
+++ backend/internal/repositories/medicine_repository..go
@@ -0,0 +1,61 @@
+package repositories
+
+import (
+	"github.com/LutfiyaAinurrahmanP/sirekam-medis-pasien/internal/models"
+	"github.com/LutfiyaAinurrahmanP/sirekam-medis-pasien/internal/validators"
+	"gorm.io/gorm"
+)
+
+type MedicineRepository interface {
+	Create(medicine *models.Medicine) error
+	Update(medicine *models.Medicine) error
+	Delete(id uint) error
+	HardDelete(id uint) error
+	Restore(id uint) error
+
+	FindByID(id uint) (*models.Medicine, error)
+	FindAll(query *validators.ListMedicineQuery) ([]models.Medicine, error)
+	FindAllDelete(query *validators.ListMedicineQuery) ([]models.Medicine, error)
+}
+
+type medicineRepository struct {
+  db *gorm.DB
+}
+
+func NewMedicineRepository(db *gorm.DB) MedicineRepository{
+	return &medicineRepository{
+		db: db,
+	}
+}
+
+func (r *medicineRepository) Create(medicine *models.Medicine) error {
+        panic("not implemented") // TODO: Implement
+}
+
+func (r *medicineRepository) Update(medicine *models.Medicine) error {
+        panic("not implemented") // TODO: Implement
+}
+
+func (r *medicineRepository) Delete(id uint) error {
+        panic("not implemented") // TODO: Implement
+}
+
+func (r *medicineRepository) HardDelete(id uint) error {
+        panic("not implemented") // TODO: Implement
+}
+
+func (r *medicineRepository) Restore(id uint) error {
+        panic("not implemented") // TODO: Implement
+}
+
+func (r *medicineRepository) FindByID(id uint) (*models.Medicine, error) {
+        panic("not implemented") // TODO: Implement
+}
+
+func (r *medicineRepository) FindAll(query *validators.ListMedicineQuery) ([]models.Medicine, error) {
+        panic("not implemented") // TODO: Implement
+}
+
+func (r *medicineRepository) FindAllDelete(query *validators.ListMedicineQuery) ([]models.Medicine, error) {
+        panic("not implemented") // TODO: Implement
+}
