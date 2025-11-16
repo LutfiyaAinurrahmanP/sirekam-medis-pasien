783832743365cf845061795a5935407db20dc527
lutfiya ainurrahman <lutfiyapr.creative@gmail.com>
2025-11-16 22:04:08 +0700

    feat: add test_type_repository.go

---
+++ backend/internal/repositories/test_type_repository.go
@@ -0,0 +1,67 @@
+package repositories
+
+import (
+	"github.com/LutfiyaAinurrahmanP/sirekam-medis-pasien/internal/models"
+	"github.com/LutfiyaAinurrahmanP/sirekam-medis-pasien/internal/validators"
+	"gorm.io/gorm"
+)
+
+type TestTypeRepository interface {
+	Crete(testType *models.TestType) error
+	Update(testType *models.TestType) error
+	Delete(id uint) error
+	HardDelete(id uint) error
+	Restore(id uint) error
+
+	FindById(id uint) (*models.TestType, error)
+	FindAll(query *validators.ListTestTypeQuery) ([]models.TestType, int64, error)
+	FindAllDelete(query *validators.ListTestTypeQuery) ([]models.TestType, int64, error)
+
+	ExistsByCode(testTypeCode string) (bool, error)
+}
+
+type testTypeRepository struct {
+  db *gorm.DB
+}
+
+func NewTestTypeRepository(db *gorm.DB) TestTypeRepository{
+	return &testTypeRepository{
+		db: db,
+	}
+}
+
+func (r *testTypeRepository) Crete(testType *models.TestType) error {
+        panic("not implemented") // TODO: Implement
+}
+
+func (r *testTypeRepository) Update(testType *models.TestType) error {
+        panic("not implemented") // TODO: Implement
+}
+
+func (r *testTypeRepository) Delete(id uint) error {
+        panic("not implemented") // TODO: Implement
+}
+
+func (r *testTypeRepository) HardDelete(id uint) error {
+        panic("not implemented") // TODO: Implement
+}
+
+func (r *testTypeRepository) Restore(id uint) error {
+        panic("not implemented") // TODO: Implement
+}
+
+func (r *testTypeRepository) FindById(id uint) (*models.TestType, error) {
+        panic("not implemented") // TODO: Implement
+}
+
+func (r *testTypeRepository) FindAll(query *validators.ListTestTypeQuery) ([]models.TestType, int64, error) {
+        panic("not implemented") // TODO: Implement
+}
+
+func (r *testTypeRepository) FindAllDelete(query *validators.ListTestTypeQuery) ([]models.TestType, int64, error) {
+        panic("not implemented") // TODO: Implement
+}
+
+func (r *testTypeRepository) ExistsByCode(testTypeCode string) (bool, error) {
+        panic("not implemented") // TODO: Implement
+}
